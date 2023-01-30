52d04fc7de3a91f86c252a860783d073bf99fd85
patrickrasabonihantana2 <patrickrasabonihantana2@gmail.com>
2023-01-30 05:56:09 +0300

    garage front nav, foot

---
+++ src/app/pages/garage/voiture/voiture.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { VoitureRoutingModule } from './voiture-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    VoitureRoutingModule
+  ]
+})
+export class VoitureModule { }
